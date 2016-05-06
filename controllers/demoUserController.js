c1aeb21d831963f55ce05cc3fdc7bdd2fa8bab5b
unknown <tamar.twena@gmail.com>
2016-05-07 02:25:42 +0300

    initial commit

---
+++ controllers/demoUserController.js
@@ -0,0 +1,27 @@
+﻿// Load required packages
+var DemoUser = require('../models/demoUser');
+
+// Create endpoint /api/users for POST
+exports.postUsers = function (req, res) {
+    var user = new DemoUser({
+        username: req.body.username,
+        password: req.body.password
+    });
+    
+    user.save(function (err) {
+        if (err)
+            res.send(err);
+        
+        res.json({ message: 'New beer drinker added to the locker room!' });
+    });
+};
+
+// Create endpoint /api/users for GET
+exports.getUsers = function (req, res) {
+    DemoUser.find(function (err, users) {
+        if (err)
+            res.send(err);
+        
+        res.json(users);
+    });
+};
---
+++ server.js
@@ -0,0 +1,53 @@
+﻿// Load required packages
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var userController = require('./controllers/demoUserController.js');
+var recipeController = require('./controllers/recipeController.js');
+var authController = require('./controllers/authenticationController.js');
+
+
+// Connect to the recipelocker MongoDB
+mongoose.connect('mongodb://localhost:27017/recipes');
+
+
+//var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI;
+//mongoose.connect(connectionString);
+
+
+
+// Create our Express application
+var app = express();
+
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+// Use environment defined port or 3000
+var port = process.env.PORT || 3000;
+
+// Create our Express router
+var router = express.Router();
+
+
+// Create endpoint handlers for /recipes
+router.route('/recipes')
+  .post(authController.isAuthenticated, recipeController.postRecipes)
+  .get(authController.isAuthenticated, recipeController.getRecipes);
+
+// Create endpoint handlers for /recipes/:recipe_id
+router.route('/recipes/:recipe_id')
+  .get(authController.isAuthenticated, recipeController.getRecipe)
+  .put(authController.isAuthenticated, recipeController.putRecipe)
+  .delete(authController.isAuthenticated, recipeController.deleteRecipe);
+
+
+
+// Register all our routes with /api
+app.use('/api', router);
+
+// Start the server
+app.listen(port);
+console.log('Demoe port ' + port);
